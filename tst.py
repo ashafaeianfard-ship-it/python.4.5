94414f42282bbd4f817b6f2fd94d538a24fe024c
ashafaeianfard-ship-it <ashafaeianfard@gmail.com>
2026-09-12 12:53:45 +0330

    Add files via upload

---
+++ tst.py
@@ -0,0 +1,11 @@
+user=input("user name:")
+passw=input("password:")
+
+if user=="arman":
+    if passw=="1234":
+        print("welcome")
+    else:
+        print("Dadashe passwrdeto yadet nist")
+else:
+    print("ki hasti asan")
+    
---
+++ tst2.py
@@ -0,0 +1,6 @@
+colors={"red","maroon","khaki","lemon","yellow","pink"}
+new=[]
+
+for i in range(5,-1,-1):
+    new.append(colors[i])
+    print(new)
